44faaebbe6976c6197a7d0fd568a604309db271f
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-09-16 10:02:12 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+inquirer
+    .prompt([
+      { name: 'nome', message: 'Qual o seu nome?' },
+      { name: 'idade', message: 'Qual a sua idade?' },
+    ])
+
+    .then((answers) => {
+        const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade}`
+        console.log(chalk.bgYellow.black(response))
+    })
+
+    .catch((err) => {
+    console.log(err)
+  })
