d1abb6e0a553d8af18145ba3683143c9a57ddc39
MATHEUS SILVA DA CRUZ <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-10-10 11:46:18 -0300

    Arruma atividade 2

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+//npm i chalk 
+//npm i inquirer
+//node index.js
+
 const chalk = require('chalk');
 
-const nota = 10;
+// const nota = 10;
 
-if (nota >= 7) {
-  console.log(chalk.green('Aprovado'));
-} else {
-  console.log(chalk.red('Reprovado'))
-}
+// if (nota >= 7) {
+//   console.log(chalk.green('Aprovado'));
+// } else {
+//   console.log(chalk.red('Reprovado'))
+// }
 
-// const inquirer = require('inquirer')
+const inquirer = require('inquirer')
 
-// inquirer
-//     .prompt([
-//       { name: 'nome', message: 'Qual o seu nome?' },
-//       { name: 'idade', message: 'Qual a sua idade?' },
-//     ])
+inquirer
+    .prompt([
+      { name: 'nome', message: 'Qual o seu nome?' },
+      { name: 'idade', message: 'Qual a sua idade?' },
+    ])
 
-//     .then((answers) => {
-//         const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade}`
-//         console.log(chalk.bgYellow.black(response))
-//     })
+    .then((answers) => {
+        const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade}`
+        console.log(chalk.bgYellow.black(response))
+    })
 
-//     .catch((err) => {
-//     console.log(err)
-//   })
+    .catch((err) => {
+    console.log(err)
+  })
